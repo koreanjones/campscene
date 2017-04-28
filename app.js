60907be449aa82d687834be76921c7a58bd2e96e
Justin Jones <charles.justin.jones@gmail.com>
2017-04-28 01:15:13 +0000

    add mongolab database

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
  
-var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+//var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+var url = process.env.DATABASEURL ||  "mongodb://koreanjones:burl73@ds123381.mlab.com:23381/campscene";
 mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
